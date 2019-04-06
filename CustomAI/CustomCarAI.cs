3f08d7f2cc685d3f113bd4e09f8c44044a9b35df
pcfantasy <pcfantasy@live.cn>
2019-04-06 16:59:41 +0800

    runtime optimize
    runtime optimize

--- CustomAI/CustomCarAI.cs
+++ CustomAI/CustomCarAI.cs
@@ -25,65 +25,70 @@ namespace TrafficCongestionReport.CustomAI
             VehicleManager instance = Singleton<VehicleManager>.instance;
             PathManager pathMan = Singleton<PathManager>.instance;
             NetManager netManager = Singleton<NetManager>.instance;
-            if (vehicleID < 16384)
+            uint currentFrameIndex = Singleton<SimulationManager>.instance.m_currentFrameIndex;
+            int num4 = (int)(currentFrameIndex & 255u);
+            if (((num4 >> 4) & 15u) == (vehicleID & 15u))
             {
-                Vehicle vehicle = instance.m_vehicles.m_buffer[vehicleID];
-                uint pathId = vehicle.m_path;
-                if (pathId >= 262144)
+                if (vehicleID < 16384)
                 {
-                    DebugLog.LogToFileOnly("Invaid pathId = " + pathId.ToString());
-                    pathId = 0;
-                }
+                    Vehicle vehicle = instance.m_vehicles.m_buffer[vehicleID];
+                    uint pathId = vehicle.m_path;
+                    if (pathId >= 262144)
+                    {
+                        DebugLog.LogToFileOnly("Invaid pathId = " + pathId.ToString());
+                        pathId = 0;
+                    }
 
-                if (pathId != 0)
+                    if (pathId != 0)
+                    {
+                        byte finePathPosIndex = vehicle.m_pathPositionIndex;
+                        byte lastPathOffset = vehicle.m_lastPathOffset;
+                        PathUnit.Position currentPosition;
+                        if (!pathMan.m_pathUnits.m_buffer[pathId].GetPosition(finePathPosIndex >> 1, out currentPosition))
+                        {
+                            //DebugLog.LogToFileOnly("Error: no currentPosition");
+                            return;
+                        }
+
+                        if (currentPosition.m_segment >= 36864)
+                        {
+                            DebugLog.LogToFileOnly("Invaid m_segment = " + currentPosition.m_segment.ToString());
+                            currentPosition.m_segment = 0;
+                        }
+
+                        if (currentPosition.m_segment != 0)
+                        {
+                            if (currentPosition.m_lane < netManager.m_segments.m_buffer[currentPosition.m_segment].Info.m_lanes.Length)
+                            {
+                                float speedLimit = netManager.m_segments.m_buffer[currentPosition.m_segment].Info.m_lanes[currentPosition.m_lane].m_speedLimit;
+                                float realSpeed = (float)Math.Sqrt(vehicle.GetLastFrameVelocity().x * vehicle.GetLastFrameVelocity().x + vehicle.GetLastFrameVelocity().y * vehicle.GetLastFrameVelocity().y + vehicle.GetLastFrameVelocity().z * vehicle.GetLastFrameVelocity().z);
+                                float tempNum = 1f;
+                                if (speedLimit != 0)
+                                {
+                                    tempNum = (realSpeed / speedLimit * 8f);
+                                }
+
+                                if (tempNum > 0.5f)
+                                {
+                                    tempNum = 0.5f;
+                                }
+
+                                tempNum = (0.5f - tempNum) * 64f;
+                                int noise;
+                                float num9 = 1f + vehicle.CalculateTotalLength(vehicleID, out noise);
+                                MainDataStore.trafficBuffer[currentPosition.m_segment] = (ushort)Mathf.Min((int)MainDataStore.trafficBuffer[currentPosition.m_segment] + (Mathf.RoundToInt(num9 * 2.5f) * tempNum), 65535);
+                            }
+                            else
+                            {
+                                DebugLog.LogToFileOnly("Error: invalid currentPosition.m_lane = " + currentPosition.m_lane.ToString() + "Length = " + netManager.m_segments.m_buffer[currentPosition.m_segment].Info.m_lanes.Length.ToString());
+                            }
+                        }
+                    }
+                }
+                else
                 {
-                    byte finePathPosIndex = vehicle.m_pathPositionIndex;
-                    byte lastPathOffset = vehicle.m_lastPathOffset;
-                    PathUnit.Position currentPosition;
-                    if (!pathMan.m_pathUnits.m_buffer[pathId].GetPosition(finePathPosIndex >> 1, out currentPosition))
-                    {
-                        //DebugLog.LogToFileOnly("Error: no currentPosition");
-                        return;
-                    }
-
-                    if (currentPosition.m_segment >= 36864)
-                    {
-                        DebugLog.LogToFileOnly("Invaid m_segment = " + currentPosition.m_segment.ToString());
-                        currentPosition.m_segment = 0;
-                    }
-
-                    if (currentPosition.m_segment != 0)
-                    {
-                        if (currentPosition.m_lane < netManager.m_segments.m_buffer[currentPosition.m_segment].Info.m_lanes.Length)
-                        {
-                            float speedLimit = netManager.m_segments.m_buffer[currentPosition.m_segment].Info.m_lanes[currentPosition.m_lane].m_speedLimit;
-                            float realSpeed = (float)Math.Sqrt(vehicle.GetLastFrameVelocity().x * vehicle.GetLastFrameVelocity().x + vehicle.GetLastFrameVelocity().y * vehicle.GetLastFrameVelocity().y + vehicle.GetLastFrameVelocity().z * vehicle.GetLastFrameVelocity().z);
-                            float tempNum = 1f;
-                            if (speedLimit != 0)
-                            {
-                                tempNum = (realSpeed / speedLimit * 8f);
-                            }
-
-                            if (tempNum > 0.5f)
-                            {
-                                tempNum = 0.5f;
-                            }
-
-                            tempNum = (0.5f - tempNum) * 4f;
-                            int noise;
-                            float num9 = 1f + vehicle.CalculateTotalLength(vehicleID, out noise);
-                            MainDataStore.trafficBuffer[currentPosition.m_segment] = (ushort)Mathf.Min((int)MainDataStore.trafficBuffer[currentPosition.m_segment] + (Mathf.RoundToInt(num9 * 2.5f) * tempNum), 65535);
-                        }
-                        else
-                        {
-                            DebugLog.LogToFileOnly("Error: invalid currentPosition.m_lane = " + currentPosition.m_lane.ToString() + "Length = " + netManager.m_segments.m_buffer[currentPosition.m_segment].Info.m_lanes.Length.ToString());
-                        }
-                    }
+                    DebugLog.LogToFileOnly("Error: invalid vehicleID = " + vehicleID.ToString());
                 }
-            }
-            else
-            {
-                DebugLog.LogToFileOnly("Error: invalid vehicleID = " + vehicleID.ToString());
             }
         }
     }
